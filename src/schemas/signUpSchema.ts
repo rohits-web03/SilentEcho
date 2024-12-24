4817d79be31ad042a00495cb65933cf2b6e53b72
rohits-web03 <rohit.dey302003@gmail.com>
2024-12-25 02:46:45 +0530

    Add zod schemas

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -1,15 +1,16 @@
-import { z } from "zod";
+import { z } from 'zod';
+
+export const usernameValidation = z
+    .string()
+    .min(2, 'Username must be at least 2 characters')
+    .max(20, 'Username must be no more than 20 characters')
+    .regex(/^[a-zA-Z0-9_]+$/, 'Username must not contain special characters');
 
 export const signUpSchema = z.object({
-    username: z
-        .string()
-        .min(3, { message: "Username must be at least 3 characters long"})
-        .max(20, { message: "Username must be at most 20 characters long"})
-        .regex(/^[a-zA-Z0-9]+$/, { message: "Username must not contain special Characters"}),
-    email: z
-        .string()
-        .email({message:"Invalid email address"}),
+    username: usernameValidation,
+
+    email: z.string().email({ message: 'Invalid email address' }),
     password: z
         .string()
-        .min(6, { message: "Password must be at least 6 characters long"})
+        .min(6, { message: 'Password must be at least 6 characters' }),
 });
