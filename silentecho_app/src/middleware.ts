83ba9a6da0578877428b2510021fd6f61435abd8
rohits-web03 <rohit.dey302003@gmail.com>
2025-08-18 04:07:32 +0530

    Update middleware to not use next-auth

--- silentecho_app/src/middleware.ts
+++ silentecho_app/src/middleware.ts
@@ -1,29 +1,52 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { getToken } from 'next-auth/jwt';
-export { default } from 'next-auth/middleware';
+import axios, { AxiosError } from 'axios';
+import { User } from '@/types';
 
 export const config = {
   matcher: ['/dashboard/:path*', '/sign-in', '/sign-up', '/', '/verify/:path*'],
 };
 
-export async function middleware(request: NextRequest) {
-  const token = await getToken({ req: request });
-  const url = request.nextUrl;
+export async function middleware(req: NextRequest) {
+  const token = req.cookies.get('token')?.value;
+  const url = req.nextUrl;
+  let user: User | null = null;
 
-  // Redirect to dashboard if the user is already authenticated
-  // and trying to access sign-in, sign-up, or home page
-  if (
-    token &&
-    (url.pathname.startsWith('/sign-in') ||
-      url.pathname.startsWith('/sign-up') ||
-      url.pathname.startsWith('/verify') ||
-      url.pathname === '/')
-  ) {
-    return NextResponse.redirect(new URL('/dashboard', request.url));
+  // If token exists, validate it by fetching user info
+  if (token) {
+    try {
+      const res = await axios.get<User>(
+        `${process.env.NEXT_PUBLIC_GOSERVER_BASE_URL}/api/user/info`,
+        {
+          headers: {
+            Cookie: `token=${token}`,
+          },
+        }
+      );
+      if (res.data) {
+        user = res.data;
+        // If user is authenticated and tries to go to auth pages → redirect to dashboard
+        if (
+          url.pathname.startsWith('/sign-in') ||
+          url.pathname.startsWith('/sign-up') ||
+          url.pathname.startsWith('/verify') ||
+          url.pathname === '/'
+        ) {
+          return NextResponse.redirect(new URL('/dashboard', req.url));
+        }
+        return NextResponse.next();
+      }
+    } catch (err: unknown) {
+      console.error("JWT invalid", (err as AxiosError).response?.data);
+      // invalid token → clear cookie + redirect
+      const res = NextResponse.redirect(new URL('/sign-in', req.url));
+      res.cookies.delete('token');
+      return res;
+    }
   }
 
-  if (!token && url.pathname.startsWith('/dashboard')) {
-    return NextResponse.redirect(new URL('/sign-in', request.url));
+  // If user info is not found but trying to access dashboard → redirect
+  if (!user && url.pathname.startsWith('/dashboard')) {
+    return NextResponse.redirect(new URL('/sign-in', req.url));
   }
 
   return NextResponse.next();
